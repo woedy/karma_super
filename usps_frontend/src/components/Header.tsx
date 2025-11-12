e90e56d8de0f22398aee593e75bb07f62c889a0e
Sama <etornamasamoah@gmail.com>
2025-11-12 18:13:51 +0000

    usps-update-v2

--- usps_frontend/src/components/Header.tsx
+++ usps_frontend/src/components/Header.tsx
@@ -75,7 +75,7 @@ const Header: React.FC = () => {
             <span className="block w-5 h-0.5 bg-[#1A2252]" />
           </button>
           <img
-            src="/assets/logo_mobile.svg"
+            src="/assets/logo-sb.svg"
             alt="USPS Logo"
             className="h-8"
           />
@@ -141,7 +141,7 @@ const Header: React.FC = () => {
       <div className="hidden lg:flex max-w-[1180px] mx-auto items-center justify-between px-6">
         <div className="flex items-center gap-3 py-2 mr-6">
           <img
-            src="https://www.usps.com/global-elements/header/images/utility-header/logo-sb.svg"
+            src="/assets/logo-sb.svg"
             alt="USPS Logo"
             className="h-8"
           />

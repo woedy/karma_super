915a19d0ca28690d8a397040e64b67764c7f2065
Sama <etornamasamoah@gmail.com>
2026-02-04 04:24:37 +0000

    php only - v1

--- karma_backend/core/Bots/blockedsBacked/blocked_referer.py
+++
@@ -1,9 +0,0 @@
-bot_BLOCKED_REFERERS = [
-    "safebrowsing.google.com",
-    "urlscan.io",
-    "phishtank.com",
-    "spamhaus.org",
-    "spamhaus.com",
-    "virustotal.com",
-    "google.com",
-]
